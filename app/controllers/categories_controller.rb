5dc3ab064f0c8588704b7684ede2edf28411308a
kizzonia <chibuikeforune@hotmail.com>
2016-10-22 19:50:13 -0400

    done

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,5 @@
 class CategoriesController < ApplicationController
   before_action :set_category, only: [:show, :edit, :update, :destroy]
-    before_action :authenticate_user!, except: [:index, :show]
 
 
   def index
