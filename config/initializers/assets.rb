38e8a9d70af4ccf7d583c0f0647ec982956ffd3c
kizzonia <chibuikeforune@hotmail.com>
2016-10-22 17:04:21 -0400

    done

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,4 +10,3 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
  Rails.application.config.assets.precompile += %w( search.js )
  Rails.application.config.assets.precompile += %w( ckeditor/* )
-Rails.application.config.assets.precompile += [/^[-_a-zA-Z0-9]*\..*/]
