010d02fc88a05e54c6bbcd9cbb997fe13c5af7fa
kizzonia <chibuikeforune@hotmail.com>
2016-10-22 06:30:04 -0400

    mvc

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,19 @@
+class CommentsController < ApplicationController
+
+  def create
+    @blog = Blog.find(params[:blog_id])
+    @comments = Blog.comments.create(params[:comment].permit(:name, :body))
+    redirect_to blog_path(@blog)
+  end
+
+  def destroy
+    @blog = Blog.find(params[:blog_id])
+    @comment = @blog.comments.find(params[:id])
+    @comment.destroy
+
+    redirect_to blog_path(@blog)
+
+  end
+
+
+end
--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,2 +1,4 @@
 class Blog < ActiveRecord::Base
+  has_many :comments
+   belongs_to :category
 end
