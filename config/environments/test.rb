15a7ffe26c98098ab6299b38a4fcb518fdb8d3c2
kizzonia <chibuikeforune@hotmail.com>
2016-10-30 19:28:14 -0400

    nigga did

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -11,7 +11,17 @@ Rails.application.configure do
   # just for the purpose of running a single test. If you are using a tool that
   # preloads Rails for running tests, you may have to set it to true.
   config.eager_load = false
-
+  config.paperclip_defaults = {
+    storage: :s3,
+    s3_credentials: {
+      bucket: ENV.fetch('picszongo'),
+      access_key_id: ENV.fetch('AKIAI6K7KR36YLLYTVUA'),
+      secret_access_key: ENV.fetch('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
+'),
+      s3_region: ENV.fetch('US_Standard'),
+    }
+  }
+    #
   # Configure static file server for tests with Cache-Control for performance.
   config.serve_static_files   = true
   config.static_cache_control = 'public, max-age=3600'
