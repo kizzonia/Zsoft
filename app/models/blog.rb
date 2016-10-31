b26996155b10ceae4105b38de38b0cead48c07a5
kizzonia <chibuikeforune@hotmail.com>
2016-10-30 21:35:25 -0400

    nigga did

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -3,4 +3,5 @@ class Blog < ActiveRecord::Base
    belongs_to :category
    has_attached_file :thumbnail, styles: { large: "1200x1000", medium: "600x500" }
   validates_attachment_content_type :thumbnail, content_type: /\Aimage\/.*\z/
+  Paperclip::Attachment.default_options[:s3_region] = 'us-west-2'
 end
