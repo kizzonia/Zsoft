0f8c7bb211a1630f357ff37b92ae8b66845677de
kizzonia <chibuikeforune@hotmail.com>
2016-11-25 11:26:12 -0500

    Services front and back

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,5 +1,5 @@
 class Blog < ActiveRecord::Base
-  has_many :comments
+  has_many :comments, dependent: :destroy
    belongs_to :category
    has_attached_file :thumbnail, styles: { large: "1200x1000", medium: "600x500" }
   validates_attachment_content_type :thumbnail, content_type: /\Aimage\/.*\z/
