9e9b966f90ea4034710216bd5dab9733bbb802e7
kizzonia <chibuikeforune@hotmail.com>
2016-11-25 00:08:55 -0500

    test10

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,7 +18,7 @@
 //= require ckeditor/init
 //= require owl.carousel.min
 //= require jquery_ujs
-//= require jquery.isotop
+//= require jquery.isotop3
 //= require custom-scripts
 //= require mousescroll
 //= require jquery.inview
