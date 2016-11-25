0cbe75f6e5fbf0abf027cee9ac4a6d346ba921bf
kizzonia <chibuikeforune@hotmail.com>
2016-11-24 21:51:09 -0500

    test10

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,8 @@
 class ApplicationController < ActionController::Base
-  before_filter :has_header
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def has_header
-    @has_header = true
-end
+
 end
