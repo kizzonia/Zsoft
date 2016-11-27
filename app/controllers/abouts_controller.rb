98af16b7e3c2145f14ee480bf18655b02e729f14
kizzonia <chibuikeforune@hotmail.com>
2016-11-27 15:44:32 -0500

    fonr

--- app/controllers/abouts_controller.rb
+++ app/controllers/abouts_controller.rb
@@ -69,6 +69,6 @@ class AboutsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def about_params
-      params.require(:about).permit(:name, :portfolio, :website, :email, :signature, :contact, :hobby)
+      params.require(:about).permit(:name, :portfolio, :website, :email, :signature, :contact, :hobby, :thumbnail)
     end
 end
