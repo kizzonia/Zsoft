38ce36fd9b923f9af534edd5a5279d8a9a9f7926
kizzonia <chibuikeforune@hotmail.com>
2016-12-17 12:38:08 -0500

    finishing up zongosoft

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require ckeditor/init
+
 //= require jquery
-//= require ckeditor/init
 //= require jquery_ujs
 //= require bootstrap
 //= require turbolinks
