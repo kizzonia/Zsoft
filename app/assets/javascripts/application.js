e8dc6a5a4718627e7ead011e2663634ab5cc0023
kizzonia <chibuikeforune@hotmail.com>
2016-12-17 14:23:58 -0500

    finishing up zongosoft

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,7 +14,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap
-//= require jquery_ujs
 //= require turbolinks
+//= require ckeditor/init
 //= require custom-scripts
 //= require main
--- config/initializers/gibbon.rb
+++ config/initializers/gibbon.rb
@@ -1,3 +1,3 @@
-Gibbon::API.api_key = "5513c75ffe19ea78d150b8cf8d7dc727-us14"
+Gibbon::Request.new(api_key: "5513c75ffe19ea78d150b8cf8d7dc727-us14")
 Gibbon::API.timeout = 15
 Gibbon::API.throws_exceptions = false
