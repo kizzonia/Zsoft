3a1f064f3f122ea53bd9b46d5ae4082abd6145ca
kizzonia <chibuikeforune@hotmail.com>
2016-12-18 07:30:52 -0500

    finishing up zongosoft

--- Gemfile
+++ Gemfile
@@ -11,6 +11,7 @@ gem 'therubyracer', platforms: :ruby
 gem 'rails_12factor'
 gem 'jquery-turbolinks'
 gem 'sendgrid'
+gem "wysiwyg-rails"
 gem 'gibbon'#, git: 'https://github.com/amro/gibbon.git'
 gem 'ckeditor'
 gem 'jquery-rails'
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require ckeditor/init
-
+//= require froala_editor.min.js
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap
