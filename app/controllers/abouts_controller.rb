fd8fb7850f7787fef1adb6b23ed8093bc128c419
kizzonia <chibuikeforune@hotmail.com>
2016-12-20 18:52:59 -0500

    done

--- app/controllers/abouts_controller.rb
+++ app/controllers/abouts_controller.rb
@@ -43,7 +43,7 @@ class AboutsController < ApplicationController
   def update
 
       if @about.update(params[:about].permit(:name, :portfolio, :website, :email, :signature, :contact, :hobby, :thumbnail))
-      redirect_to @about
+      redirect_to abouts_url
       else
         render 'edit'
       end
